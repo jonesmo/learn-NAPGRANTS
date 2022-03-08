b683a5ada9b647ad0613f5003c769d8309ca80e0
Molly Jones <molly.jones@spantree.net>
2022-03-08 15:11:39 -0600

    Add pagination

--- graphql/types/Link.ts
+++ graphql/types/Link.ts
@@ -1,4 +1,4 @@
-import { extendType, objectType } from "nexus";
+import { arg, extendType, intArg, objectType, stringArg } from "nexus";
 import { User } from "./User";
 
 export const Link = objectType({
@@ -25,13 +25,81 @@ export const Link = objectType({
   },
 });
 
+export const Edge = objectType({
+  name: "Edge",
+  definition(t) {
+    t.string("cursor");
+    t.field("node", {
+      type: Link,
+    });
+  },
+});
+
+export const PageInfo = objectType({
+  name: "PageInfo",
+  definition(t) {
+    t.string("endCursor");
+    t.boolean("hasNextPage");
+  },
+});
+
+export const Response = objectType({
+  name: "Response",
+  definition(t) {
+    t.field("pageInfo", { type: PageInfo });
+    t.list.field("edges", {
+      type: Edge,
+    });
+  },
+});
+
 export const LinksQuery = extendType({
   type: "Query",
   definition(t) {
-    t.nonNull.list.field("links", {
-      type: "Link",
-      resolve(_parent, _args, ctx) {
-        return ctx.prisma.link.findMany();
+    t.field("links", {
+      type: "Response",
+      args: {
+        first: intArg(),
+        after: stringArg(),
+      },
+      async resolve(_, args, ctx) {
+        let queryResults = null;
+
+        if (args.after) {
+          queryResults = await ctx.prisma.link.findMany({
+            take: args.first,
+            skip: 1,
+            cursor: {
+              id: args.after,
+            },
+          });
+        } else {
+          queryResults = await ctx.prisma.link.findMany({
+            take: args.first,
+          });
+        }
+
+        if (queryResults.length > 0) {
+          const lastLinkInResults = queryResults[queryResults.length - 1];
+          const myCursor = lastLinkInResults.id;
+          const secondQueryResults = await ctx.prisma.link.findMany({
+            take: args.first,
+            cursor: {
+              id: myCursor,
+            },
+          });
+          const result = {
+            pageInfo: {
+              endCursor: myCursor,
+              hasNextPage: secondQueryResults.length >= args.first,
+            },
+            edges: queryResults.map((link) => ({
+              cursor: link.id,
+              node: link,
+            })),
+          };
+          return result;
+        }
       },
     });
   },
